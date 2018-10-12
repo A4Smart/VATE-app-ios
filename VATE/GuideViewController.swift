ca484ccf9593025120a43766c6dd087166f6bb49
Marco Fincato <marcofincato777@gmail.com>
2018-10-12 16:18:09 +0200

    Restarting with the iOS app

---
+++ VATE/GuideViewController.swift
@@ -0,0 +1,13 @@
+//
+//  GuideViewController.swift
+//  VATE
+//
+//  Created by Marco Fincato on 12/10/2018.
+//  Copyright © 2018 A4Smart. All rights reserved.
+//
+
+import UIKit
+
+class GuideViewController: UIViewController {
+    
+}
---
+++ VATE/ProximityViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ViewController.swift
+//  VATE
+//
+//  Created by Marco Fincato on 08/10/2018.
+//  Copyright © 2018 A4Smart. All rights reserved.
+//
+
+import UIKit
+import os
+import CoreLocation
+
+
+class ProximityViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func buttonHandler(_ sender: UIButton) {
+        os_log("prova")
+        locationManager.requestAlwaysAuthorization()
+        locationManager.delegate = self
+        monitorBeacons()
+    }
+    
+    func monitorBeacons() {
+        os_log("starting monitoring")
+        if CLLocationManager.isMonitoringAvailable(for:
+            CLBeaconRegion.self) {
+            // Match all beacons with the specified UUID
+            let proximityUUID = UUID(uuidString:
+                "B9407F30-F5F8-466E-AFF9-25556B57FE6D")
+            let beaconID = "it.a4smart.beacons"
+            
+            // Create the region and begin monitoring it.
+            let region = CLBeaconRegion(proximityUUID: proximityUUID!,
+                                        identifier: beaconID)
+            self.locationManager.startRangingBeacons(in: region)
+            os_log("monitoring started")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        os_log("being called..")
+        if region is CLBeaconRegion {
+            // Start ranging only if the feature is available.
+            if CLLocationManager.isRangingAvailable() {
+                os_log("starting ranging")
+                manager.startRangingBeacons(in: region as! CLBeaconRegion)
+            }
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager,
+                         didRangeBeacons beacons: [CLBeacon],
+                         in region: CLBeaconRegion) {
+        
+        if beacons.count > 0 {
+        
+            
+            let nearestBeacon = beacons[0]
+            let major = CLBeaconMajorValue(truncating: nearestBeacon.major)
+            let minor = CLBeaconMinorValue(truncating: nearestBeacon.minor)
+            
+            os_log("Beacon: %s", type: .debug, "minor: \(minor.description), major: \(major.description)")
+        }
+    }
+    
+}
+
